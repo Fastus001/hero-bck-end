f1ba7ba1dc4e5673f2f4426ca469e866f7981ddb
Fastus <fastus6666@gamil.com>
2021-04-30 15:32:18 +0200

    Created Race entity. Closes #7.

---
+++ src/main/java/pl/fastus/wfrpg/hero/domain/race/Race.java
@@ -0,0 +1,31 @@
+package pl.fastus.wfrpg.hero.domain.race;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@Entity
+public class Race {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ElementCollection
+    private List<String> professionName;
+
+    @ElementCollection
+    private List<String> talentNames;
+
+    private String freeTalents;
+}
